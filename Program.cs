ab6631b71ee069d62183078e6cd4cc9f4be0542e
mynkov <mynkovas@mail.ru>
2023-03-04 19:46:33 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using AngleSharp;
+using QuickType;
+
+var checkPriviledgedStocks = true;
+File.Delete("output.txt");
+
+
+
+
+var bigCompanies = await GetCompanies("https://smart-lab.ru/q/index_stocks/IMOEX/order_by_issue_capitalization/desc/", checkPriviledgedStocks);
+//PrintCompanies(bigCompanies, "Big companies:");
+
+// https://www.tinkoff.ru/api/invest-gw/ca-portfolio/api/v1/user/portfolio/pie-chart
+var text = File.ReadAllText("Stocks.json");
+var stocks = JsonSerializer.Deserialize<Stocks>(text, Converter.Settings);
+
+var agg = new List<Aggregate>();
+foreach (var company in bigCompanies)
+{
+        var myStock = stocks.Issuers.FirstOrDefault(x => x.InstrumentInfo != null && x.InstrumentInfo.Any(x => x.Ticker == company.Ticker));
+        agg.Add(new Aggregate(company, myStock ?? new CurrencyElement{ ValueAbsolute = new TotalAmountCurrency() }));
+}
+
+PrintAggregates(agg, "Big companies:");
+return;
+
+
+var allCompanies = await GetCompanies("https://smart-lab.ru/q/index_stocks/MOEXBMI/order_by_issue_capitalization/desc/", checkPriviledgedStocks);
+PrintCompanies(allCompanies, "All companies:");
+
+var exceptCompanies = allCompanies.Except(bigCompanies, new CompanyComparer()).ToList();
+PrintCompanies(exceptCompanies, "Except companies:");
+
+double? prevIndex = 0;
+PrintCompanies(exceptCompanies.Select(x =>
+{
+    var isHole = x.Index - prevIndex > 1;
+    prevIndex = x.Index;
+    return new { Company = x, IsHole = isHole };
+}).Where(x => x.IsHole).Select(x => x.Company).ToList(), "Increment except companies holes:");
+
+PrintCompanies(bigCompanies.Where(x => Math.Abs(x.PercentDiff) >= 0.005).OrderByDescending(x => x.PercentDiff).ToList(), "Big companies percent diff:");
+PrintCompanies(allCompanies.Where(x => Math.Abs(x.PercentDiff) >= 0.005).OrderByDescending(x => x.PercentDiff).ToList(), "All companies percent diff:");
+
+Console.ReadLine();
+
+static async Task<List<Company>> GetCompanies(string url, bool checkPriviledgedStocks)
+{
+    var document = await BrowsingContext.New(Configuration.Default.WithDefaultLoader()).OpenAsync(url);
+
+    var titles = document.QuerySelectorAll("table tr td:nth-child(3)").Select(m => m.TextContent).ToList();
+    var tickers = document.QuerySelectorAll("table tr td:nth-child(3) a").Select(m => m.GetAttribute("Href").Replace("/forum/", "")).ToList();
+    var percents = document.QuerySelectorAll("table tr td:nth-child(4)").Select(m => m.TextContent).ToList();
+    var prices = document.QuerySelectorAll("table tr td:nth-child(7)").Select(m => m.TextContent).ToList();
+    var caps = document.QuerySelectorAll("table tr td:nth-child(12)").Select(m => m.TextContent).ToList();
+
+    HttpClient client = new HttpClient();
+
+    var list = titles.Select((x, i) => new
+    {
+        Title = x,
+        Cap = double.Parse(caps[i].Replace(" ", "")),
+        Percent = double.Parse(percents[i].Replace("%", "")) / 100,
+        Ticker = tickers[i],
+        Price = double.Parse(prices[i])
+    }).ToList();
+
+    var capSum = list.GroupBy(x => x.Cap).Sum(x => x.Key);
+
+    var companies = list.GroupBy(x => x.Cap).Select((x, i) => new
+            Company(
+                i + 1,
+                string.Join(", ", x.Select(y => x.Count() > 1 ? $"{y.Title} ({y.Percent:P2}, {y.Price}Р)" : y.Title)),
+                checkPriviledgedStocks && client.GetStringAsync($"https://www.tinkoff.ru/invest/stocks/{x.First().Ticker}P/").Result.Contains("Купить акции") ? $"{x.First().Ticker}P" : x.First().Ticker,
+                x.Key,
+                x.Last().Price,
+                x.Sum(s => s.Percent),
+                x.Key / capSum,
+                x.Key / capSum - x.Sum(s => s.Percent)
+                )).ToList();
+
+    return companies;
+}
+
+static void PrintAggregates(List<Aggregate> aggs, string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine();
+
+    File.AppendAllText("output.txt", title + "\n\n");
+
+    var myTotalCap = aggs.Sum(x => x.myStock.ValueAbsolute.Value);
+    var totalBuyRub = 0.0;
+    var totalBuyCount = 0;
+
+    foreach (var agg in aggs)
+    {
+        var company = agg.company;
+        var myStock = agg.myStock;
+        var myStockCap = myStock.ValueAbsolute.Value;
+
+        var myPercent = myStockCap / myTotalCap;
+        var myDiff = company.NewPercent - myPercent;
+
+        //var myDiffRubOld = sum * myDiff;
+
+        var restCap = myTotalCap - myStockCap;
+        var restPercent = 1 - company.NewPercent;
+        var afterBuySum = restCap / restPercent;
+        var myStockTargerCap = afterBuySum - restCap;
+        var myDiffRub = myStockTargerCap - myStockCap;
+
+        var amountToBuy = myDiffRub / company.Price;
+        var amountToBuyText = amountToBuy > 0 ?
+        amountToBuy >= 1000 ?
+         amountToBuy >= 1000000 ? $"{amountToBuy / 1000000:0}M" : $"{amountToBuy / 1000:0}K" :
+          $"{amountToBuy:0}" : "--";
+
+        if(myDiffRub > 0)
+        {
+          totalBuyRub += myDiffRub;
+          totalBuyCount++;
+        }
+
+        var line = $"{company.Index}\t{company.NewPercent:P2}\t\t{myPercent:P2}\t{myDiff:+0.00%;-0.00%}\t{myStockCap / 1000:0}\t\t{company.Percent:P2}\t{company.PercentDiff:+0.00%;-0.00%}\t\t{company.Cap:0.00}\thttps://www.tinkoff.ru/invest/stocks/{company.Ticker}\t{amountToBuyText}\t{myDiffRub:+0;-0}\t{company.Price / 1000:0.000}\t{company.Title}";
+        Console.WriteLine(line);
+        File.AppendAllText("output.txt", line + "\n");
+    }
+
+
+    var totalCapMessage = $"\nMy total cap: {myTotalCap/1000000:0.000}";
+    Console.Write(totalCapMessage);
+    File.AppendAllText("output.txt", totalCapMessage);
+
+    var totalBuyRubMessage = $"\nMy total buy: {totalBuyRub/1000:0}K ({totalBuyCount}шт)";
+    Console.Write(totalBuyRubMessage);
+    File.AppendAllText("output.txt", totalBuyRubMessage);
+
+    Console.WriteLine();
+    Console.WriteLine();      
+    File.AppendAllText("output.txt", "\n\n");
+}
+
+static void PrintCompanies(List<Company> companies, string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine();
+
+    File.AppendAllText("output.txt", title + "\n\n");
+
+    foreach (var company in companies)
+    {
+        PrintCompany(company);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    File.AppendAllText("output.txt", "\n\n");
+}
+
+static void PrintCompany(Company company)
+{
+    var line = $"{company.Index}\t{company.NewPercent:P2}\t{company.Percent:P2}\t{company.PercentDiff:+0.00%;-0.00%}\t{company.Cap:0.00}\thttps://www.tinkoff.ru/invest/stocks/{company.Ticker}\t{company.Price / 1000:0.000}\t{company.Title}";
+    Console.WriteLine(line);
+    File.AppendAllText("output.txt", line + "\n");
+}
+
+public record Company(int Index, string Title, string Ticker, double Cap, double Price, double Percent, double NewPercent, double PercentDiff);
+
+public record Aggregate(Company company, CurrencyElement myStock);
+
+public class CompanyComparer : IEqualityComparer<Company>
+{
+    public bool Equals(Company? x, Company? y)
+    {
+        return x?.Ticker == y?.Ticker;
+    }
+
+    public int GetHashCode([DisallowNull] Company obj)
+    {
+        return obj.Ticker.GetHashCode();
+    }
+}
---
+++ Stocks.generated.cs
@@ -0,0 +1,275 @@
+﻿namespace QuickType
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using System.Globalization;
+
+    public partial class Stocks
+    {
+        [JsonPropertyName("currencies")]
+        public List<CurrencyElement> Currencies { get; set; }
+
+        [JsonPropertyName("instruments")]
+        public List<CurrencyElement> Instruments { get; set; }
+
+        [JsonPropertyName("issuers")]
+        public List<CurrencyElement> Issuers { get; set; }
+
+        [JsonPropertyName("sectors")]
+        public List<CurrencyElement> Sectors { get; set; }
+
+        [JsonPropertyName("totalAmountCurrency")]
+        public TotalAmountCurrency TotalAmountCurrency { get; set; }
+
+        [JsonPropertyName("totalAmountCurrencyQty")]
+        public long TotalAmountCurrencyQty { get; set; }
+
+        [JsonPropertyName("totalAmountInstrument")]
+        public TotalAmountCurrency TotalAmountInstrument { get; set; }
+
+        [JsonPropertyName("totalAmountInstrumentQty")]
+        public long TotalAmountInstrumentQty { get; set; }
+
+        [JsonPropertyName("totalAmountIssuer")]
+        public TotalAmountCurrency TotalAmountIssuer { get; set; }
+
+        [JsonPropertyName("totalAmountIssuerQty")]
+        public long TotalAmountIssuerQty { get; set; }
+
+        [JsonPropertyName("totalAmountSector")]
+        public TotalAmountCurrency TotalAmountSector { get; set; }
+
+        [JsonPropertyName("totalAmountSectorQty")]
+        public long TotalAmountSectorQty { get; set; }
+    }
+
+    public partial class CurrencyElement
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyName("instrumentInfo")]
+        public List<InstrumentInfo> InstrumentInfo { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("valueAbsolute")]
+        public TotalAmountCurrency ValueAbsolute { get; set; }
+
+        [JsonPropertyName("valueRelative")]
+        public double ValueRelative { get; set; }
+    }
+
+    public partial class InstrumentInfo
+    {
+        [JsonPropertyName("positionType")]
+        public PositionType PositionType { get; set; }
+
+        [JsonPropertyName("positionUid")]
+        public Guid PositionUid { get; set; }
+
+        [JsonPropertyName("ticker")]
+        public string Ticker { get; set; }
+    }
+
+    public partial class TotalAmountCurrency
+    {
+        [JsonPropertyName("currency")]
+        public CurrencyEnum Currency { get; set; }
+
+        [JsonPropertyName("value")]
+        public double Value { get; set; }
+    }
+
+    public enum PositionType { Etf, Stock };
+
+    public enum CurrencyEnum { Rub };
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerOptions Settings = new(JsonSerializerDefaults.General)
+        {
+            Converters =
+            {
+                PositionTypeConverter.Singleton,
+                CurrencyEnumConverter.Singleton,
+                new DateOnlyConverter(),
+                new TimeOnlyConverter(),
+                IsoDateTimeOffsetConverter.Singleton
+            },
+        };
+    }
+
+    internal class PositionTypeConverter : JsonConverter<PositionType>
+    {
+        public override bool CanConvert(Type t) => t == typeof(PositionType);
+
+        public override PositionType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            switch (value)
+            {
+                case "etf":
+                    return PositionType.Etf;
+                case "stock":
+                    return PositionType.Stock;
+            }
+            throw new Exception("Cannot unmarshal type PositionType");
+        }
+
+        public override void Write(Utf8JsonWriter writer, PositionType value, JsonSerializerOptions options)
+        {
+            switch (value)
+            {
+                case PositionType.Etf:
+                    JsonSerializer.Serialize(writer, "etf", options);
+                    return;
+                case PositionType.Stock:
+                    JsonSerializer.Serialize(writer, "stock", options);
+                    return;
+            }
+            throw new Exception("Cannot marshal type PositionType");
+        }
+
+        public static readonly PositionTypeConverter Singleton = new PositionTypeConverter();
+    }
+
+    internal class CurrencyEnumConverter : JsonConverter<CurrencyEnum>
+    {
+        public override bool CanConvert(Type t) => t == typeof(CurrencyEnum);
+
+        public override CurrencyEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            if (value == "RUB")
+            {
+                return CurrencyEnum.Rub;
+            }
+            throw new Exception("Cannot unmarshal type CurrencyEnum");
+        }
+
+        public override void Write(Utf8JsonWriter writer, CurrencyEnum value, JsonSerializerOptions options)
+        {
+            if (value == CurrencyEnum.Rub)
+            {
+                JsonSerializer.Serialize(writer, "RUB", options);
+                return;
+            }
+            throw new Exception("Cannot marshal type CurrencyEnum");
+        }
+
+        public static readonly CurrencyEnumConverter Singleton = new CurrencyEnumConverter();
+    }
+    
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        private readonly string serializationFormat;
+        public DateOnlyConverter() : this(null) { }
+
+        public DateOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "yyyy-MM-dd";
+        }
+
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return DateOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+
+    public class TimeOnlyConverter : JsonConverter<TimeOnly>
+    {
+        private readonly string serializationFormat;
+
+        public TimeOnlyConverter() : this(null) { }
+
+        public TimeOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "HH:mm:ss.fff";
+        }
+
+        public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return TimeOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+
+    internal class IsoDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+    {
+        public override bool CanConvert(Type t) => t == typeof(DateTimeOffset);
+
+        private const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK";
+
+        private DateTimeStyles _dateTimeStyles = DateTimeStyles.RoundtripKind;
+        private string? _dateTimeFormat;
+        private CultureInfo? _culture;
+
+        public DateTimeStyles DateTimeStyles
+        {
+            get => _dateTimeStyles;
+            set => _dateTimeStyles = value;
+        }
+
+        public string? DateTimeFormat
+        {
+            get => _dateTimeFormat ?? string.Empty;
+            set => _dateTimeFormat = (string.IsNullOrEmpty(value)) ? null : value;
+        }
+
+        public CultureInfo Culture
+        {
+            get => _culture ?? CultureInfo.CurrentCulture;
+            set => _culture = value;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+        {
+            string text;
+
+
+            if ((_dateTimeStyles & DateTimeStyles.AdjustToUniversal) == DateTimeStyles.AdjustToUniversal
+                || (_dateTimeStyles & DateTimeStyles.AssumeUniversal) == DateTimeStyles.AssumeUniversal)
+            {
+                value = value.ToUniversalTime();
+            }
+
+            text = value.ToString(_dateTimeFormat ?? DefaultDateTimeFormat, Culture);
+
+            writer.WriteStringValue(text);
+        }
+
+        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string? dateText = reader.GetString();
+
+            if (string.IsNullOrEmpty(dateText) == false)
+            {
+                if (!string.IsNullOrEmpty(_dateTimeFormat))
+                {
+                    return DateTimeOffset.ParseExact(dateText, _dateTimeFormat, Culture, _dateTimeStyles);
+                }
+                else
+                {
+                    return DateTimeOffset.Parse(dateText, Culture, _dateTimeStyles);
+                }
+            }
+            else
+            {
+                return default(DateTimeOffset);
+            }
+        }
+
+
+        public static readonly IsoDateTimeOffsetConverter Singleton = new IsoDateTimeOffsetConverter();
+    }
+}
