bafc5987e1dd2671f839bdec6bd5fbf1011af683
a.mynkov <mynkovas@mail.ru>
2026-07-03 14:29:55 +0300

    add

--- Program.cs
+++ Program.cs
@@ -295,10 +295,10 @@ static MyTinkoffStock GetMyTinkoffStock(string ticker, TinkoffPortfolios.Tinkoff
             AddStocks(9490);
             break;
         case "LKOH":
-            AddStocks(636);
+            AddStocks(659);
             break;
         case "ROSN":
-            AddStocks(4177);
+            AddStocks(4504);
             break;
         case "SBERP":
             AddStocks(19377);
